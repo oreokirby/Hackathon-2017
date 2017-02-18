010f4b2e8c6a82e320565d054424543f1f2bc7dd
oreokirby <hardcorezeldafan@gmail.com>
2017-02-18 11:42:33 -0500

    Basic Movement

---
+++ Git Forcefield/Git Assets/FirstPerson.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPerson : MonoBehaviour {
+
+	private Vector3 moveVector;
+	private Vector3 lastMove;
+	public float speed = 8;
+	public float jumpforce = 8;
+	public float gravity = 25;
+	public float senseH = 9.0f;
+	private float verticalVelocity = 0f;
+	private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// Point the object in the direction of the mouse
+		transform.Rotate(0, Input.GetAxis ("Mouse X") * senseH, 0);
+
+		moveVector = Vector3.zero;
+		moveVector.x = Input.GetAxis ("Horizontal");
+		moveVector.z = Input.GetAxis ("Vertical");
+
+		if (controller.isGrounded) {
+			verticalVelocity = -1;
+
+			if (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.Space)) {
+				verticalVelocity = jumpforce;
+			}
+		}
+
+		else {
+			verticalVelocity -= gravity * Time.deltaTime;
+			moveVector = lastMove;
+		}
+			
+		moveVector.y = 0;
+		moveVector.Normalize ();
+		moveVector *= speed;
+		moveVector.y = verticalVelocity;
+
+		controller.Move (moveVector * Time.deltaTime);
+		lastMove = moveVector;
+	}
+}
